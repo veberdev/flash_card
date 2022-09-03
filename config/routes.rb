166cf7ab877ece1c966b8c25069a35905c0dad0a
veber <weslusisa@gmail.com>
2022-09-03 16:50:27 -0300

    initializing dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  # devise_scope :user do
-  #   root 'devise/registrations#new'
-  # end
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ resources :dashboard, only: :index
+
 end
