7f6cfa3ee411f085dc09658d7ce4335ebbf5b195
veber <weslusisa@gmail.com>
2022-09-04 12:02:45 -0300

    redirect to dashboard when user logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
+  before_action :user_logged_in?
+
   def index
     @user = User.first
+  end
+
+  private
+
+  def user_logged_in?
+    redirect_to dashboard_index_path if current_user.present?
   end
 end
