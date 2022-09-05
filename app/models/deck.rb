916a4ae66fb31c0ceef5bdb0ae5864e4f76cdb33
veber <weslusisa@gmail.com>
2022-09-05 16:54:03 -0300

    add some validations and tests for decks

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,4 +4,8 @@ class Deck < ApplicationRecord
   has_many :questions
 
   has_one_attached :deck_image, dependent: :destroy
+
+  validates :name, length: {maximum: 14}, presence: true
+  validates :description, presence: true
+
 end
