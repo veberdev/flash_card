cb45412cec7745ef166e3d63dc066bd2e09dce11
veber <weslusisa@gmail.com>
2022-09-09 11:38:23 -0300

    fix closebtn modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,16 +2,10 @@ import { Controller } from '@hotwired/stimulus'
 import { enter, leave, toggle } from 'el-transition'
 
 export default class extends Controller {
-  static targets = ['closeButton'];
 
   connect() {
     document.getElementById(`modal-${this.element.dataset.modalTriggerId}-wrapper`).addEventListener('click', (event) => {
       this.closeModal(event, this.element.dataset.modalTriggerId)
-    });
-    this.closeButtonTarget.addEventListener('click', () => {
-      leave(document.getElementById(`modal-${this.element.dataset.modalTriggerId}-wrapper`));
-      leave(document.getElementById(`modal-${this.element.dataset.modalTriggerId}-backdrop`));
-      leave(document.getElementById(`modal-${this.element.dataset.modalTriggerId}-panel`));
     });
   }
 
