6979644e08ec11bb972df80e1b51cd6f5b67c639
veber <weslusisa@gmail.com>
2022-09-09 07:20:56 -0300

    remove some input fields on change question type on new question form

--- app/javascript/controllers/questions/new_question_controller.js
+++ app/javascript/controllers/questions/new_question_controller.js
@@ -5,10 +5,11 @@ export default class extends Controller {
   static targets = ['questionTypeField', 'wrongAnswersFields'];
 
   connect() {
-    
     this.questionTypeFieldTarget.addEventListener('change', () => {
-      console.log("im here at new question controller")
       if (this.questionTypeFieldTarget.value == 'user_correction') {
+        this.wrongAnswersFieldsTarget.childNodes[3].value = ""
+        this.wrongAnswersFieldsTarget.childNodes[5].value = ""
+        this.wrongAnswersFieldsTarget.childNodes[7].value = ""
         leave(this.wrongAnswersFieldsTarget);
       } else {
         enter(this.wrongAnswersFieldsTarget);
